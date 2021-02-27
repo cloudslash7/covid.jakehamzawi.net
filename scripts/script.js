ebe9b45d73c7fa9b61297d66f9d6164116922a0e
Jake Hamzawi <jedhamzawi@protonmail.com>
2021-02-27 12:36:53 -0800

    reorganized using async

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,43 @@
-function getCountries() {
+document.forms["search"].elements["searchBox"].focus();
+async function getCountryData() {
     const url = "https://api.covid19api.com/countries";
-    fetch(url)
+    return fetch(url)
         .then(response => {
             return response.json();
-        }).then(json => {
-            let countries = new Array();
-            for (let i = 0; i < json.length; i++) {
-                countries.push(json[i].Country);
-            }
-            $( "#searchBox").autocomplete({
-                source: countries,
-                minLength: 2,
-                delay: 500
-            });
         }).catch(error => {
             console.log(error);
+            return error;
         })
+}
+
+async function getCountryNames() {
+    getCountryData().then(response => {
+        let countryNames = new Array();
+        for (let i = 0; i < response.length; i++) {
+            countryNames.push(response[i].Country);
+        }
+        $( "#searchBox").autocomplete({
+            source: countryNames,
+            minLength: 2,
+            delay: 500
+        })
+        return countryNames;
+    })
+    .catch(error => console.log(error))
+}
+
+
+function getData() {
+    getCountryNames().then(response => {
+        document.getElementById("submit").addEventListener("click", event => {
+            console.log("test")
+            event.preventDefault();
+            const value = document.getElementById("searchBox").value;
+            if (value === "") return;
+            console.log(response);
+            console.log(countries.find(item => item.Country === "Ukraine"));
+            const url = "https://https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z";
+        })
+    })
+    .catch(error => console.log(error))
 }
